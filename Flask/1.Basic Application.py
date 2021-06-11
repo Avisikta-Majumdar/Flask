e3858193751e0fea84c0c89008d5fbf82c81c669
Avisikta Majumdar <47344480+Avisikta-Majumdar@users.noreply.github.com>
2021-06-11 20:05:03 +0530

    Add files via upload

---
+++ Flask/1.Basic Application.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def Welcome():
+    return "This is my first flask application"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
