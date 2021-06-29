49b836e9b7511b5dd5ea5f1016857753777cea74
Avisikta Majumdar <47344480+Avisikta-Majumdar@users.noreply.github.com>
2021-06-29 19:46:28 +0530

    Add files via upload

--- Flask/6. Flask Debug Mode/main.py
+++ Flask/6. Flask Debug Mode/main.py
@@ -5,13 +5,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def Home(name):
+def Home():
     return "<h1>Welcome to This Tutorial</h1>"
 
 @app.route('/info/<name>/')
 def DynamicAllocation(name):
-    return "<h1>Welcome {}, to This Tutorial</h1>".format(name)
+    return "the 10th element of this name {}".format(name[9])
 
 
 if __name__ == '__main__':
     app.run(debug = True)
+
+
+    '''
+    Put debug = True at the time of development ,
+    not at deployment
+    
+    If debug is True this means it will show / trace the error 
+    '''
